5db960df0cc2b1b5db8499cbb43130d35a7bba5c
David Deller <david@horizon-nigh.org>
2010-06-22 17:45:35 -0400

    Remove code that tries to focus/unfocus Search button correctly.
    It just doesn't work reliably enough. I left the inert CSS styling, in case I can find a way to make it work later.

--- Searchmarks.safariextension/inject.js
+++ Searchmarks.safariextension/inject.js
@@ -14,20 +14,5 @@ SearchmarksJQuery(document).ready(
       }
     );
 
-    // the 'window' object in the extension toolbar does not work as you would expect -
-    // it fires focus and blur events based on the toolbar, not the entire window.
-    // therefore, we detect those events in this injected script instead, and dispatch
-    // messages to the extension toolbar.
-    SearchmarksJQuery(window).bind("focus", function()
-      {
-        safari.self.tab.dispatchMessage("window-focus", this.href);
-      }
-    );
-    SearchmarksJQuery(window).bind("blur", function()
-      {
-        safari.self.tab.dispatchMessage("window-blur", this.href);
-      }
-    );
-
   }
 );
